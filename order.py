7c6a3354a0b189b09ab6d4297e94ffac92c494e2
vitek <vitek@gmail.com>
2025-12-21 00:04:38 +0300

    This_is_Days_from_today

--- order.py
+++ order.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-def get_days_from_today(date_str) -> int:
-    target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-    current_date = datetime.today().date()
-    delta = target_date - current_date
-    return delta.days
 
+def get_days_from_today(date):
+    target_date = datetime.strptime(date, "%Y-%m-%d").date()
+    current_date = datetime.today().date()
+    return (current_date - target_date).days
+if __name__ == "__main__":
+    print(f"Дней с 2023-12-25: {get_days_from_today('2023-12-25')}")
+    print(f"Дней до 2025-12-19: {get_days_from_today('2025-12-19')}")
+    today_str = datetime.today().strftime("%Y-%m-%d")
+    print(f"Дней с сегодня ({today_str}): {get_days_from_today(today_str)}")
---
+++ test.py
@@ -0,0 +1,2 @@
+user_input = input("ВВеди команду: ")
+print(f"ВЫ ВВели: {user_input}")
