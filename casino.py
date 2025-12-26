9fffc0076065bfc796e726221f2a0c9a90f4bbe6
vitek <vitek@gmail.com>
2025-12-27 00:35:06 +0300

    casino_random

---
+++ casino.py
@@ -0,0 +1,14 @@
+#Щоб виграти головний приз лотереї, 
+# необхідний збіг кількох номерів на лотерейному квитку з числами, що випали випадковим чином і в певному діапазоні під час чергового тиражу. 
+# Наприклад, необхідно вгадати шість чисел від 1 до 49 чи п'ять чисел від 1 до 36 тощо.
+
+
+
+import random  
+ 
+def get_numbers_ticket (min, max, quantity): 
+    ticket_numbers = set()
+    while len(ticket_numbers) < quantity: 
+        ticket_numbers.add(random.randint(min, max)) 
+    return ticket_numbers 
+print("Вы получили следующие номера билета:", get_numbers_ticket(1, 1000, 6)) 
